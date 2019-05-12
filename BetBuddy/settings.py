841161c1e8f32d14607e71700780edc9d82b1a2a
David Brown <browndj24@googlemail.com>
2019-05-12 21:51:38 +0100

    installed and added gunicorn + django-debug-toolbar and alloowed hosts in settings for dev

--- BetBuddy/settings.py
+++ BetBuddy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q7^a!roxxsid*+bq3&!_2uwzda$+cm(@=e&m*rw@1orgg=t^(='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
