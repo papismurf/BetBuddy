17265060e96da607c612655703251c8fa8866add
David Brown <browndj24@googlemail.com>
2019-05-12 14:33:34 +0100

    configured static directories in settings

--- BetBuddy/settings.py
+++ BetBuddy/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'betbuddy',
         'USER': 'smurf',
-        'PASSWORD': 'password',
+        'PASSWORD': '!?Lav301',
         'HOST': 'localhost',
         'PORT': '',
     }
@@ -124,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
